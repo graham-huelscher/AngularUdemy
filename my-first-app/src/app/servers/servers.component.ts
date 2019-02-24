466e48a21538e0d7dedd9676e712771636cc7c69
graham-huelscher <graham.huelscher@gmail.com>
2019-02-23 16:13:25 -0800

    file restructuring

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -2,16 +2,32 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverName = 'testserver';
+  serverCreated = false;
+  servers = ['Test', 'Test 2'];
 
-  constructor() { }
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
+  }
+
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName)
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;    
   }
 
 }
